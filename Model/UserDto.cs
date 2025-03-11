d6e9dd40668389d136d1222ad1aa56ec381b0b8b
HoangAnh163634 <147035735+HoangAnh163634@users.noreply.github.com>
2025-03-11 15:33:29 +0700

    Update User's Function
    Update

---
+++ Model/UserDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class UserDto
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Mobile is required")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits")]
+    public long Mobile { get; set; }
+
+    [Required(ErrorMessage = "Address is required")]
+    public string Address { get; set; }
+
+    public string? GoogleId { get; set; }
+}
--- Pages/Users/Manage.cshtml.cs
+++ Pages/Users/Manage.cshtml.cs
@@ -1,11 +1,9 @@
-using api_flms_service.Model;
-using api_flms_service.ServiceInterface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace api_flms_service.Pages.Users
 {
-    public class IndexModel : PageModel
+    public class ManageModel : PageModel
     {
         public void OnGet()
         {
