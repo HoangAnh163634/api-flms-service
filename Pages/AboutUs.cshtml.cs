a9f5de55f64833d8cc244c5b7e72dd7c60c15087
HoangAnh163634 <147035735+HoangAnh163634@users.noreply.github.com>
2025-03-27 11:41:13 +0700

    Update UI

---
+++ Pages/AboutUs.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace api_flms_service.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
