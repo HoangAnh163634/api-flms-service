20b71f8488ab7b2cd468622c8cecd52195011219
Nguyen Tien Dung <dungnthe176106@fpt.edu.vn>
2025-03-29 09:07:13 +0700

    Update AIChatController.cs

--- Controllers/AIChatController.cs
+++ Controllers/AIChatController.cs
@@ -22,7 +22,16 @@ namespace api_flms_service.Controllers
         [HttpPost("SendMessage")]
         public async Task<IActionResult> Send([FromBody] ChatRequest request)
         {
-            var apiKey = _config["OpenAI:ApiKey"];
+            //string Encrypt(string text, string key) => new string(text.Select((c, i) => (char)(c ^ key[i % key.Length])).ToArray());
+            string Decrypt(string text, string key) => new string(text.Select((c, i) => (char)(c ^ key[i % key.Length])).ToArray());
+
+            
+            
+
+            var apiKey = Decrypt(
+                "\u0017\u001eC\u0017\u001c\u0005\u000eX_\u0004\b\u0006=<\u0002!\u0018Z\a\r'6\u001a\t \0=\u001e\u0019+\u0012\u0017\u0005\bV;\u0012??\v,>U\u0004(\u0012C\u001eP#\u000f&*\u001f\n\0\u0005\v\a5\a\u0014\u0004,^>3B</[\"\u00141\u0006\u0003\u001eR\f\u001f(\r:Y&\u0019\f\f( \u0013\u001a(1X\r7\u0016+\u001f\a9/2&\u0016\u0019\u0003%\u001f\u000f%\u0005\bW\u001f!8\n;\n\u001bX\n\0\b4\r\u0006?\r[.;\f+(\u0019\u000f\u0006!\u0013\u0004%/\u001b\u001a5CG\u0011\u0001\u00190?\u0006Q\u001a\v\0\u00189T4",
+                 "dungnj"
+                );
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
 
             var body = new
